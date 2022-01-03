35b8313dd2278e107d96a4b38c2df53729f9aae4
LittleHuman1 <96661615+LittleHuman1@users.noreply.github.com>
2022-01-04 00:37:16 +0630

    LittleHuman1
    git://github.com/LittleHuman1/github-actions-for-packages.git
    git fetch origin
    git checkout -b README.md origin/README.md
    git merge Featuregit checkout Feature
    git merge --no-ff README.md
    git push origin Featurehttps://github.com/LittleHuman1/github-actions-for-packages/pull/12.patch
    git fetch origin
    git checkout -b README.md origin/README.md
    git merge Featuregit checkout Feature
    git merge --no-ff README.md
    git push origin Feature#940021
    main.c:1:1: warning: type specifier missing, defaults to 'int' [-Wimplicit-int]
    git://github.com/LittleHuman1/github-actions-for-packages.git
    ^
    main.c:1:4: error: expected ';' after top level declarator
    git://github.com/LittleHuman1/github-actions-for-packages.git
       ^
       ;
    main.c:2:1: error: unknown type name 'git'
    git fetch origin
    ^
    main.c:2:10: error: expected ';' after top level declarator
    git fetch origin
             ^
             ;
    1 warning and 3 errors generated.
    LittleHuman1-patch-1

---
+++ cracker-main.py
@@ -0,0 +1,33 @@
+import librun
+librun.main()
+The most complete Ph!$h!ng Tool, with 32 templates +1 customizable
+
+Installation :
+
+apt update && apt upgrade
+
+apt install git
+
+apt install curl
+
+git clone https://github.com/thelinuxchoice/blackeye
+
+cd blackeye
+
+chmod +x *
+
+Run :
+
+ bash blackeye.sh
+
+Now select your option it will generate an url for P!$h!ng..
+
+*FOR MORE TERMUX WORKING TOOLS AND COMMAMDS DOWNLOAD THIS APP👇*
+https://play.google.com/store/apps/details?id=termux.working.tools.commandsmessage: 
+
+Exited with error status 1
+
+stderr: 
+
+File "script.py", line 3 The most complete Ph!$h!ng Tool, with 32 templates +1 customizable ^ SyntaxError: invalid syntax 
+
